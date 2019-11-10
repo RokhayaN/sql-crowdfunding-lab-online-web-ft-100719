a914306df45f88ef26c944bc9fbe220c09c7670d
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-10 05:28:02 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,15 +36,19 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   
-  SELECT users.name && SUM pledges.amount AS pledges_summed_amount
-  FROM users
-  JOIN pledges ON users.id = pledges.id 
-  GROUP BY name 
-  ORDER BY pledges_summed_amount
+  SELECT users.name, SUM(pledges.amount)
+  FROM users 
+  JOIN pledges ON users.id = pledges.user_id 
+  GROUP BY users.id
+  ORDER BY SUM(pledges.amount), users.name;
+end
+  
+  
+  
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+  
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
