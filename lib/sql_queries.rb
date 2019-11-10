cf8afcda1a08a4f74c5b7b66c3d3173719cc5366
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-10 05:36:55 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,9 +43,6 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   ORDER BY SUM(pledges.amount), users.name;"
 end
   
-  
-  
-end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   
