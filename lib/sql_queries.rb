ba248f2e1f72bd8de6f40def9bfa41e19efcc226
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-10 05:40:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,7 +33,6 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   HAVING SUM(pledges.amount)-projects.funding_goal>=0;"
 end
 
-
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   
  "SELECT users.name, SUM(pledges.amount)
@@ -45,16 +44,13 @@ end
   
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  
   "SELECT projects.category, pledges.amount
   FROM projects 
   JOIN pledges ON projects.id = pledges.project_id 
   WHERE projects.category = 'music';"
-  
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  
   "SELECT projects.category, SUM(pledges.amount)
   FROM projects 
   JOIN pledges ON projects.id = pledges.project_id 
