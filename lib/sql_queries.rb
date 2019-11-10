5679c4348bf3c17101ae7aea68a0ec286442680a
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-10 05:34:45 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,36 +11,36 @@ SELECT projects.title, SUM(pledges.amount) AS total_pledge_amount_by_project
   FROM projects 
   JOIN pledges ON projects.id = pledges.project_id
   GROUP BY projects.id
-  ORDER BY projects.title ASC;
+  ORDER BY projects.title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  SELECT users.name, users.age ,SUM(pledges.amount) AS total_pledges_amount_by_users
+  "SELECT users.name, users.age ,SUM(pledges.amount) AS total_pledges_amount_by_users
   FROM users 
   JOIN pledges ON users
   GROUP BY users.id
-  ORDER BY users.name ASC;
+  ORDER BY users.name ASC;"
   
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  
-  SELECT projects.title, SUM(pledges.amount)-projects.funding_goal 
+ 
+ " SELECT projects.title, SUM(pledges.amount)-projects.funding_goal 
   FROM projects 
   JOIN pledges 
   ON projects.id = pledges.project_id
   GROUP BY projects.id
-  HAVING SUM(pledges.amount)-projects.funding_goal>=0;
+  HAVING SUM(pledges.amount)-projects.funding_goal>=0;"
 end
 
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   
-  SELECT users.name, SUM(pledges.amount)
+ "SELECT users.name, SUM(pledges.amount)
   FROM users 
   JOIN pledges ON users.id = pledges.user_id 
   GROUP BY users.id
-  ORDER BY SUM(pledges.amount), users.name;
+  ORDER BY SUM(pledges.amount), users.name;"
 end
   
   
@@ -57,8 +57,10 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "SELECT books.category, pledges.amount
-  FROM books
-  JOIN pledges ON books.id = pledges.books_id 
-  WHERE books.category = 'music';"
+  
+  "SELECT projects.category, SUM(pledges.amount)
+  FROM projects 
+  JOIN pledges ON projects.id = pledges.project_id 
+  WHERE projects.category = 'books'
+  GROUP BY projects.category;"
 end
